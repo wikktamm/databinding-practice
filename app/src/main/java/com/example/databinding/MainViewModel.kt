3e36c608d5a58264d0429b9e8070cd8918af647d
Wiktoria Tarnowska <wikktamm@gmail.com>
2020-06-12 23:17:12 +0200

    Create two way db

--- app/src/main/java/com/example/databinding/MainViewModel.kt
+++ app/src/main/java/com/example/databinding/MainViewModel.kt
@@ -1,10 +1,11 @@
 package com.example.databinding
 
+import androidx.databinding.Bindable
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class MainViewModel(val repo:FakeRepository) : ViewModel() {
+class MainViewModel(val repo: FakeRepository) : ViewModel() {
 
     private val _currentName = MutableLiveData<String>()
     val currentName: LiveData<String>
@@ -13,9 +14,22 @@ class MainViewModel(val repo:FakeRepository) : ViewModel() {
     init {
         _currentName.value = repo.getRandomName()
     }
+
     fun getRandomName() = repo.getRandomName()
 
-    fun chooseRandomName(){
+    fun chooseRandomName() {
         _currentName.value = repo.getRandomName()
+    }
+     val editTextContent = MutableLiveData<String>()
+
+    private val _editTextDisplayed = MutableLiveData<String>()
+    val editTextDisplayed: LiveData<String>
+        get() = _editTextDisplayed
+
+    fun displayEditTextContent() {
+        _editTextDisplayed.value = editTextContent.value
+    }
+    fun selectRandomNameForEditText(){
+        editTextContent.value = getRandomName()
     }
 }
